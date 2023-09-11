cb1b36e8c8e441be77258508f6ebe698051e6811
Marius Hatland <marius.hatland@student.uib.no>
2023-09-11 12:22:39 +0200

    Removed comment

--- src/main/java/no/hvl/dat250/jpa/tutorial/basicexample/Main.java
+++ src/main/java/no/hvl/dat250/jpa/tutorial/basicexample/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
   public static void main(String[] args) {
 
-    //Test
     try (EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa-tutorial");
         EntityManager em = factory.createEntityManager()) {
 
